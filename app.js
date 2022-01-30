5497dec446c7e5990a1f4dbb41b3841affc7c9b0
tecosama <taylancankose@gmail.com>
2022-01-30 19:00:06 +0300

    added

---
+++ app.js
@@ -0,0 +1,15 @@
+
+import axios from "axios";
+
+const getUserId = async(user_id) => {
+
+        const {data: user} = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`);
+        const {data: post} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
+
+        return {"user": user, "post":post};
+
+    };
+
+export default getUserId;
+
+
---
+++ index.js
@@ -0,0 +1,4 @@
+import getUserId from "./app.js";
+
+let result = await getUserId(2)
+console.log(result)
